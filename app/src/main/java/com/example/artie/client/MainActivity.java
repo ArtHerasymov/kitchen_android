411be6d96e477ae8f0e704dfbd72ae3dd6d13842
artieherasymov <artieherasymov@gmail.com>
2018-04-17 08:27:19 -0700

    Attempt to push the entire project

---
+++ app/src/main/java/com/example/artie/client/MainActivity.java
@@ -0,0 +1,36 @@
+package com.example.artie.client;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    TextView responseView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+
+    @Override
+    public void onClick(View view) {
+        Intent intent = new Intent(this, NewOrderActivity.class);
+        this.startActivity ( intent );
+    }
+}
