2351c04bf066c223d49ffe45997c0b33c532efb0
artieherasymov <artieherasymov@gmail.com>
2018-04-20 13:43:53 -0700

    Confirmation view added

--- app/src/main/java/com/example/artie/client/MainActivity.java
+++ app/src/main/java/com/example/artie/client/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.artie.client;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -86,49 +87,13 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     }
 
     public void onSubmitClick(View view) {
-        String url = "http://192.168.1.106:5000/orders/create";
-        RequestQueue MyRequestQueue = Volley.newRequestQueue(this);
-        StringRequest MyStringRequest = new StringRequest(Request.Method.POST, url, new Response.Listener<String>() {
-            @Override
-            public void onResponse(String response) {
-                //This code is executed if the server responds, whether or not the response contains data.
-                //The String 'response' contains the server's response.
-                Toast.makeText(
-                        MainActivity.this,
-                        response,
-                        Toast.LENGTH_SHORT)
-                        .show();
-                currentOrder = new Order();
-                currentOrder.setItems(itemList);
-                currentOrder.setPrice(price);
-                currentOrder.setStatus("IN_PROGRESS");
-                System.out.println("RESPONSE : " + response);
-                int id = Integer.parseInt(response);
+        setContentView(R.layout.confirm_order);
+        TextView itemsView = findViewById(R.id.confirmItems);
+        TextView priceView = findViewById(R.id.confirmPrice);
 
-                currentOrder.setId(id);
-                orderList.add(currentOrder);
-            }
-        }, new Response.ErrorListener() { //Create an error listener to handle errors appropriately.
-            @Override
-            public void onErrorResponse(VolleyError error) {
-                //This code is executed if there is an error.
-                Toast.makeText(
-                        MainActivity.this,
-                        error.getMessage(),
-                        Toast.LENGTH_SHORT)
-                        .show();
-            }
-        }) {
-            protected Map<String, String> getParams() {
-                Map<String, String> MyData = new HashMap<String, String>();
-                MyData.put("Waiter", "Johny LOL"); //Add the data you'd like to send to the server.
-                MyData.put("Items", itemList);
-                MyData.put("DiscountID", "123");
-                MyData.put("InitialPrice",String.valueOf(price));
-                return MyData;
-            }
-        };
-        MyRequestQueue.add(MyStringRequest);
+        itemsView.setText(itemList);
+        priceView.setText(String.valueOf(price));
+
     }
 
     public void onCheckClicK(View view){
@@ -177,48 +142,82 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         }
     }
 
-    public void onRefreshCick(View view){
+    public void onRefreshCick(final View view){
         TextView  curid = view.findViewById(R.id.id);
+        final TextView statusView = view.findViewById(R.id.status);
         System.out.println(curid.getText());
-//        String url = "http://192.168.1.106:5000/orders/create";
-//        RequestQueue MyRequestQueue = Volley.newRequestQueue(this);
-//        StringRequest MyStringRequest = new StringRequest(Request.Method.POST, url, new Response.Listener<String>() {
-//            @Override
-//            public void onResponse(String response) {
-//                //This code is executed if the server responds, whether or not the response contains data.
-//                //The String 'response' contains the server's response.
-//                Toast.makeText(
-//                        MainActivity.this,
-//                        response,
-//                        Toast.LENGTH_SHORT)
-//                        .show();
-//                currentOrder = new Order();
-//                currentOrder.setItems(itemList);
-//                currentOrder.setPrice(price);
-//                currentOrder.setStatus("IN_PROGRESS");
-//                currentOrder.setId(Integer.getInteger(response));
-//                orderList.add(currentOrder);
-//            }
-//        }, new Response.ErrorListener() { //Create an error listener to handle errors appropriately.
-//            @Override
-//            public void onErrorResponse(VolleyError error) {
-//                //This code is executed if there is an error.
-//                Toast.makeText(
-//                        MainActivity.this,
-//                        error.getMessage(),
-//                        Toast.LENGTH_SHORT)
-//                        .show();
-//            }
-//        }) {
-//            protected Map<String, String> getParams() {
-//                Map<String, String> MyData = new HashMap<String, String>();
-//                MyData.put("Waiter", "Johny LOL"); //Add the data you'd like to send to the server.
-//                MyData.put("Items", itemList);
-//                MyData.put("DiscountID", "123");
-//                MyData.put("InitialPrice",String.valueOf(price));
-//                return MyData;
-//            }
-//        };
-//        MyRequestQueue.add(MyStringRequest);
+        // Instantiate the RequestQueue.
+        RequestQueue queue = Volley.newRequestQueue(this);
+        String url ="http://192.168.1.106:5000/orders/details/"+curid.getText();
+
+// Request a string response from the provided URL.
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
+                new Response.Listener<String>() {
+                        @SuppressLint("SetTextI18n")
+                    @Override
+                    public void onResponse(String response) {
+                        // Display the first 500 characters of the response string.
+                            statusView.setText(response);
+                            System.out.println("RESPONSE : " + response);
+                        }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+            }
+        });
+// Add the request to the RequestQueue.
+        queue.add(stringRequest);
+    }
+
+    public void confirmButtonClick(View view) {
+        String url = "http://192.168.1.106:5000/orders/create";
+        RequestQueue MyRequestQueue = Volley.newRequestQueue(this);
+        StringRequest MyStringRequest = new StringRequest(Request.Method.POST, url, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                //This code is executed if the server responds, whether or not the response contains data.
+                //The String 'response' contains the server's response.
+                Toast.makeText(
+                        MainActivity.this,
+                        response,
+                        Toast.LENGTH_SHORT)
+                        .show();
+                currentOrder = new Order();
+                currentOrder.setItems(itemList);
+                currentOrder.setPrice(price);
+                currentOrder.setStatus("IN_PROGRESS");
+                int id = Integer.parseInt(response);
+
+                currentOrder.setId(id);
+                orderList.add(currentOrder);
+            }
+        }, new Response.ErrorListener() { //Create an error listener to handle errors appropriately.
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                //This code is executed if there is an error.
+                Toast.makeText(
+                        MainActivity.this,
+                        error.getMessage(),
+                        Toast.LENGTH_SHORT)
+                        .show();
+            }
+        }) {
+            protected Map<String, String> getParams() {
+                Map<String, String> MyData = new HashMap<String, String>();
+                MyData.put("Waiter", "Johny LOL"); //Add the data you'd like to send to the server.
+                MyData.put("Items", itemList);
+                MyData.put("DiscountID", "123");
+                MyData.put("InitialPrice",String.valueOf(price));
+                return MyData;
+            }
+        };
+        MyRequestQueue.add(MyStringRequest);
+        currentOrder = null;
+        setContentView(R.layout.activity_main);
+    }
+
+    public void onCancleButtonClick(View view){
+        this.currentOrder = null;
+        setContentView(R.layout.activity_main);
     }
 }
